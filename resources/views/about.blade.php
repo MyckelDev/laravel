aa1d841e1cf1bbe62b4e0ecd28b5d11ca8df3f4b
Michael Barrios <michaelbarrios584@gmail.com>
2022-11-09 15:05:31 -0500

    .8 plantillas de blade con herencias

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
-</head>
-<body>
-    @include('partials.navigation');
-    <h1>About</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title','About')
+
+@section('meta-description','About meta description')
+
+
+@section('content')
+    
+<h1>About</h1>
+
+
+@endsection
