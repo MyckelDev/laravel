666d678c4ec8dabd43d03ba421592d66d8fffea4
Michael Barrios <michaelbarrios584@gmail.com>
2022-11-10 12:14:51 -0500

    .10 controladores

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+$posts = [
+    ['title' => 'First post'  ],
+    ['title' => 'Second post'  ],
+    ['title' => 'Third post'  ],
+    ['title' => 'Fourth post'  ],
+];
+
 Route::view('/','welcome')->name('home');
 Route::view('/contacto','contacto')->name('contact');
-Route::view('/blog','blog')->name('blog');
+Route::view('/blog','blog',['posts' => $posts])->name('blog');
 Route::view('/about','about')->name('about'); 
