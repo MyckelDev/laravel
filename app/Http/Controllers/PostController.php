e53d0471343094a72bc227d527a9d2c9a3f1ab5a
MyckelDev <michaelbarrios584@gmail.com>
2023-01-14 15:49:24 -0500

    fix: create argumnt

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,7 +24,7 @@ class PostController extends Controller
 
     }
 
-    public function create($validated)
+    public function create()
     {
      
         return view('posts.create', ['post' => new Post]);
